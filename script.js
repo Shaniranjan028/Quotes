492035d10bef9e2d822c88e7bd2e2ab73171d1e6
shaniranjan028 <shaniranjan028@gmail.com>
2020-06-05 14:01:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const quotes = [
+  {
+    name: "Albert Einstein",
+    quote: "I have no special talent. I am only passionately curious.",
+  },
+  {
+    name: "Steve Jobs",
+    quote: "Stay hungry, stay foolish",
+  },
+  {
+    name: "Malcolm X",
+    quote: "The future belongs to those who prepare for it today.",
+  },
+  {
+    name: "Dalai Lama",
+    quote: "Be kind whenever possible. It is always possible",
+  },
+  {
+    name: "Buddha",
+    quote: "The root of suffering is attachment",
+  },
+];
+
+const quoteBtn = document.querySelector("#quoteBtn");
+const quoteAuthor = document.querySelector("#quoteAuthor");
+const quote = document.querySelector("#quote");
+
+quoteBtn.addEventListener("click", displayQuote);
+
+function displayQuote() {
+  let number = Math.floor(Math.random() * quotes.length);
+  quoteAuthor.innerHTML = quotes[number].name;
+  quote.innerHTML = quotes[number].quote;
+}
